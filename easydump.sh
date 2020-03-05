55db65269ca06023da786b74a00ceb3fb77381ba
Jean Castro <34094820+jeanvcastro@users.noreply.github.com>
2020-03-05 14:25:33 -0300

    Create easydump.sh

---
+++ easydump.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+HOST=localhost
+DATABASE=
+USER=root
+PASSWORD=
+TABELAS_IGNORADAS=(
+logs
+postbacks
+examplo
+)
+ARQUIVO=dump.sql
+
+TABELAS_IGNORADAS_STRING=''
+for TABELA in "${TABELAS_IGNORADAS[@]}"
+do :
+   TABELAS_IGNORADAS_STRING+=" --ignore-table=${DATABASE}.${TABELA}"
+done
+
+echo "Exportando estrutura..."
+mysqldump --host=${HOST} --user=${USER} --password=${PASSWORD} --single-transaction --no-data --routines ${DATABASE} > ${ARQUIVO}
+
+echo "Exportando dados..."
+mysqldump --host=${HOST} --user=${USER} --password=${PASSWORD} ${DATABASE} --single-transaction --no-create-info --skip-triggers ${TABELAS_IGNORADAS_STRING} >> ${ARQUIVO}
